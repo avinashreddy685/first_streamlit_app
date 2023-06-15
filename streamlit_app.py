d7a8e341aacd0ff86bfca0cc3c8a73903742dde4
avinashreddy685 <136658051+avinashreddy685@users.noreply.github.com>
2023-06-15 06:10:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ created the main python file
 import stramlit
 
 stramlit.title('my parents new healthy diner')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
